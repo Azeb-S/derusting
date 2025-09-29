7b6104ed256d4c2632797b9c2c497ac522cb5a87
Azeb Tesfay <azbseb@gmail.com>
2025-09-28 22:36:38 -0700

    List Practice Commit

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,18 +1,27 @@
-public class ListPractice {
+import java.util.ArrayList;
 
+public class ListPractice {
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
 
+    ArrayList<String> daysOfTheWeek = new ArrayList<String>();
+
     // Add 3 elements to the list (OK to do one-by-one)
+    daysOfTheWeek.add("Sunday");
+    daysOfTheWeek.add("Monday");
+    daysOfTheWeek.add("Tusday");
 
     // Print the element at index 1
 
+    System.out.println(daysOfTheWeek.get(1));
+
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
+    daysOfTheWeek.set(1, "Saturday");
 
     // Insert a new element at index 0 (the length of the list will change)
-
+    daysOfTheWeek.add(0, "Fryday");
     // Check whether the list contains a certain string
 
     // Iterate over the list using a traditional for-loop.
@@ -26,10 +35,12 @@ public class ListPractice {
     /*
      * Usage tip!
      * 
-     * Use a traditional for-loop when you need to use the index or you need to iterate in an
+     * Use a traditional for-loop when you need to use the index or you need to
+     * iterate in an
      * unconventional order (e.g. backwards)
      * 
-     * Otherwise, if you're iterating the in the conventional order and don't need the
+     * Otherwise, if you're iterating the in the conventional order and don't need
+     * the
      * index values a for-each loop is cleaner.
      */
   }
